e5236922b4a354caaf276444f241bef0abd9196d
Beinjamin <jauresbeinjamin@gmail.com>
2024-12-20 14:39:08 +0100

    Gestion des imports

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import os
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
+from azure.ai.contentsafety import ContentSafetyClient
+from azure.ai.contentsafety.models import TextCategory
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+from azure.ai.contentsafety.models import AnalyzeTextOptions
